60093814dd82e91603cb2ec165b7475ff6fa7249
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2023-07-04 23:34:09 +0300

    Create Assignments#01.py

---
+++ Assignments/#001 TO #010/Assignments#01.py
@@ -0,0 +1,17 @@
+# assignment = "Assignment#01"
+
+name = "Amar"
+age = 20
+country = "Egypt"
+
+
+print("Name: " + name)
+print("Age: " + str(age))
+print("Country: " + country)
+
+
+print("Hello, My Name Is  " + name + " And Iam  " + str(age) + " Years Old and I Live in  " + country + ".")
+
+print(type(name))
+print(type(age))
+print(type(country))
