3fdfde4d9e6c04f5752336b0f2a4439df7adfb52
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2023-07-07 07:36:44 +0300

    Create Assignment#008.py

---
+++ Assignments/#038 TO #040/Assignment#008.py
@@ -0,0 +1,19 @@
+name = input("What\'s Your Name ?").strip().capitalize()
+print(f"Hello {name}, Happy To See You Here.")
+print("=" * 30)
+
+age = int(input("What\'s Your Age ?"))
+print( "Hello Your Age Is Under 16, Some Articles Is Not Suitable For You" if age < 16 else  f"Hello Your Age Is {age}, All Articles Is Suitable For You" ) 
+print("=" * 30)
+
+fName =  input("What\'s Your Fisrt Name ?").strip().capitalize()
+lName  =  input("What\'s Your Last Name ?").strip().capitalize()
+
+print(f"Hello {fName} {lName:.1s}.")
+print("=" * 30)
+
+email =  input("What\'s Your Email ?").strip().capitalize()
+pos = email.index("@")
+pos_dot = email.index(".")
+
+print(f"Your Name Is {(email[:pos])}\nEmail Service Provider Is {email[pos+1:pos_dot]}\nTop Level Domain Is {email[pos_dot+1 : ]}")
