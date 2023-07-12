f38605a97d6277a5d7e0683325fcd1cb4a550423
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2023-07-13 02:37:19 +0300

    Create #011.py

---
+++ Hacker Rank/#011.py
@@ -0,0 +1,13 @@
+import textwrap
+
+def wrap(string, max_width):
+    wrapped = ""
+    for i in range(0, len(string), max_width):
+        wrapped += string[i:i+max_width] + '\n'
+    return wrapped
+
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
