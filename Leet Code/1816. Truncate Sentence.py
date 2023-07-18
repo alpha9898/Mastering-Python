f37417117bf58f54d9a4107dd07bf9711be03445
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2023-07-18 08:06:18 +0300

    Create 1816. Truncate Sentence.py

---
+++ Leet Code/1816. Truncate Sentence.py
@@ -0,0 +1,6 @@
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        words = s.split()
+        truncated_words = words[:k]
+        truncated_sentence = ' '.join(truncated_words)
+        return truncated_sentence
