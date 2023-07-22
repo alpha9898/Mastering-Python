4c9157ed41de63b19cd79e1d814cf2426faba308
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2023-07-22 21:20:09 +0300

    Create 1844. Replace All Digits with Characters.py

---
+++ Leet Code/1844. Replace All Digits with Characters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def shift(self, c: str, x: int) -> str:
+        return chr(ord(c) + x)
+
+    def replaceDigits(self, s: str) -> str:
+        result = []
+        for i in range(len(s)):
+            if i % 2 == 0:
+                result.append(s[i])
+            else:
+                result.append(self.shift(result[-1], int(s[i])))
+
+        return ''.join(result)
