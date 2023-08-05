a6648fb54683b9143e67660599e6d09cade73da9
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2023-08-05 10:58:15 +0300

    Update YOUTUPE DOWNLODER.py

--- JUST SOME QUICK APPS/YOUTUBE  DOWNLODER.py
+++ JUST SOME QUICK APPS/YOUTUBE  DOWNLODER.py
@@ -34,7 +34,7 @@ def show_video_details(yt):
 
     # Display video details
     video_info = f"Video Title: {yt.title}\nVideo Duration: {hours} hours, {minutes} minutes, {seconds} seconds\nVideo Size: {file_size:.2f} MB"
-    video_info_label = tk.Label(video_details_window, text=video_info, font=("Arial", 12), wraplength=400, justify='left', bg="#241468", fg="white")
+    video_info_label = tk.Label(video_details_window, text=video_info, font=("Arial", 12), wraplength=400, justify='left', bg="#393E41", fg="white")
     video_info_label.pack(padx=20, pady=20)
 
     # Ask for confirmation for the single video download
@@ -47,7 +47,7 @@ def show_video_details(yt):
 
         video_details_window.destroy()
 
-    confirm_button = tk.Button(video_details_window, text="Confirm Download", font=("Arial", 14), bg="#EA1179", fg="white", command=confirm_download)
+    confirm_button = tk.Button(video_details_window, text="Confirm Download", font=("Arial", 14), bg="#3F88C5", fg="white", command=confirm_download)
     confirm_button.pack(pady=10)
 
 def download_single_video():
@@ -81,7 +81,7 @@ def download_playlist():
             yt = YouTube(url)
 
             # Create a LabelFrame for the video details
-            video_frame = tk.LabelFrame(video_details_window, text=f"Video Title: {yt.title}", font=("Arial", 14), bg="#241468", fg="white")
+            video_frame = tk.LabelFrame(video_details_window, text=f"Video Title: {yt.title}", font=("Arial", 14), bg="#393E41", fg="white")
             video_frame.pack(padx=20, pady=10, fill="both", expand=True)
 
             # Get the size and duration of the video
@@ -90,15 +90,15 @@ def download_playlist():
 
             # Display video details for each video in the playlist
             video_info = f"Video Duration: {hours} hours, {minutes} minutes, {seconds} seconds\nVideo Size: {file_size:.2f} MB"
-            video_info_label = tk.Label(video_frame, text=video_info, font=("Arial", 12), wraplength=400, justify='left', bg="#241468", fg="white")
+            video_info_label = tk.Label(video_frame, text=video_info, font=("Arial", 12), wraplength=400, justify='left', bg="#393E41", fg="white")
             video_info_label.pack(padx=20, pady=5)
 
             # Download button for each video
-            download_button = tk.Button(video_frame, text="Download", font=("Arial", 14), bg="#9F0D7F", fg="white", command=lambda yt=yt: show_video_details(yt))
+            download_button = tk.Button(video_frame, text="Download", font=("Arial", 14), bg="#44BBA4", fg="white", command=lambda yt=yt: show_video_details(yt))
             download_button.pack(pady=10)
 
         # Download All Videos button
-        download_all_button = tk.Button(video_details_window, text="Download All Videos", font=("Arial", 14), bg="#9F0D7F", fg="white", command=lambda: download_all_videos_in_playlist(pl))
+        download_all_button = tk.Button(video_details_window, text="Download All Videos", font=("Arial", 14), bg="#E94F37", fg="white", command=lambda: download_all_videos_in_playlist(pl))
         download_all_button.pack(pady=10)
 
     except Exception as e:
@@ -107,19 +107,19 @@ def download_playlist():
 # Create the main window
 root = tk.Tk()
 root.title("YouTube Video Downloader")
-root.geometry("600x400")
+root.geometry("500x350")
 
-# Set the window background color to #241468 (royal blue)
-root.configure(bg="#241468")
+# Set the window background color to #393E41
+root.configure(bg="#393E41")
 
 # Create URL entry field
-url_label = tk.Label(root, text="Enter YouTube Video URL or Playlist URL:", font=("Arial", 14), bg="#241468", fg="white")
+url_label = tk.Label(root, text="Enter YouTube Video URL or Playlist URL:", font=("Arial", 14), bg="#393E41", fg="white")
 url_label.pack(pady=10)
 url_entry = tk.Entry(root, width=50, font=("Arial", 12))
 url_entry.pack(pady=5)
 
 # Create resolution choice options
-resolution_label = tk.Label(root, text="Select Video Resolution:", font=("Arial", 14), bg="#241468", fg="white")
+resolution_label = tk.Label(root, text="Select Video Resolution:", font=("Arial", 14), bg="#393E41", fg="white")
 resolution_label.pack(pady=5)
 resolution_var = tk.IntVar()
 resolution_var.set(1)
@@ -129,25 +129,27 @@ resolution_choices = [
 ]
 
 for text, val in resolution_choices:
-    resolution_radio = tk.Radiobutton(root, text=text, variable=resolution_var, value=val, font=("Arial", 12), bg="#241468", fg="white", selectcolor="#EA1179")
+    resolution_radio = tk.Radiobutton(root, text=text, variable=resolution_var, value=val, font=("Arial", 12), bg="#393E41", fg="white", selectcolor="#393E41")
     resolution_radio.pack(anchor=tk.W)
 
 # Create destination folder button
-destination_folder_button = tk.Button(root, text="Choose Destination Folder", font=("Arial", 14), bg="#F79BD3", fg="white", command=choose_destination_folder)
+destination_folder_button = tk.Button(root, text="Choose Destination Folder", font=("Arial", 14), bg="#3F88C5", fg="white", command=choose_destination_folder)
 destination_folder_button.pack(pady=5)
 
 destination_path = ""
 
 # Create download single video button
-download_single_button = tk.Button(root, text="Download Single Video", font=("Arial", 14), bg="#EA1179", fg="white", command=download_single_video)
+download_single_button = tk.Button(root, text="Download Single Video", font=("Arial", 14), bg="#22816E", fg="white", command=download_single_video)
 download_single_button.pack(pady=10)
 
 # Create download playlist button
-download_playlist_button = tk.Button(root, text="Download Playlist", font=("Arial", 14), bg="#EA1179", fg="white", command=download_playlist)
+download_playlist_button = tk.Button(root, text="Download Playlist", font=("Arial", 14), bg="#22816E", fg="white", command=download_playlist)
 download_playlist_button.pack(pady=10)
 
 # Start the main event loop
 root.mainloop()
+
+
 # ****************todo list *****************
 # todo : add download playlist
 # todo : add download audio only
